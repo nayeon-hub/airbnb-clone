53f969a80dcc59888e7e1e95540a6920d18a042c
Park Na Yeon <within197@naver.com>
2021-08-02 22:04:40 +0900

    user 드롭메뉴 UI

--- modal.js
+++ modal.js
@@ -3,10 +3,25 @@ const globalBtn = sideMenu.querySelector(".global");
 const gModalCont = document.querySelector(".global-modal-cont");
 const gModalBtn = gModalCont.querySelector("button");
 
+const userBtn = sideMenu.querySelector(".user-user");
+const userDropCont = document.querySelector(".user-dropMenu-cont");
+
 globalBtn.addEventListener("click", () => {
   gModalCont.classList.remove("hiding");
 });
 
 gModalBtn.addEventListener("click", () => {
   gModalCont.classList.add("hiding");
+});
+
+userBtn.addEventListener("click", () => {
+  if (userDropCont.classList[1] === "hiding") {
+    userDropCont.classList.remove("hiding");
+  } else {
+    userDropCont.classList.add("hiding");
+  }
+});
+
+userBtn.addEventListener("focusout", () => {
+  userDropCont.classList.add("hiding");
 });
